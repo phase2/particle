11fc012d143581b8451679954ece60d3287c10af
Evan Lovely <evan@evanlovely.com>
2015-06-04 14:36:49 -0700

    adding new feature that allows regression tests to get cleaned out. creating new baselines for all changes.

--- grunt-tasks/regression-qa/regression-qa.js
+++ grunt-tasks/regression-qa/regression-qa.js
@@ -23,7 +23,7 @@ module.exports = function (grunt, config) {
           viewportSize: [1280, 800]
         },
         src: [
-          'pattern-lab/source/_patterns/**/*.test.js'
+          config.regressionTestRoot + '**/*.test.js'
         ]
       }
     },
@@ -31,16 +31,57 @@ module.exports = function (grunt, config) {
     clean: {// https://www.npmjs.com/package/grunt-contrib-clean
       preRegressionQA: [
         'baselines',
-        'pattern-lab/source/_patterns/**/results/*.{diff,fail}.png'
+        config.regressionTestRoot + '**/results/*.{diff,fail}.png'
       ],
       postRegressionQA: [
-        'pattern-lab/source/_patterns/**/baselines/*.{diff,fail}.png',
-        'pattern-lab/source/_patterns/**/results/*.png',
-        '!pattern-lab/source/_patterns/**/results/*.{diff,fail}.png'
+        config.regressionTestRoot + '**/baselines/*.{diff,fail}.png',
+        config.regressionTestRoot + '**/results/*.png',
+        '!' + config.regressionTestRoot + '**/results/*.{diff,fail}.png'
       ]
     }
 
-
   });
+
+  // grunt testClean:featured-item will remove the baseline folder adjacent to featured-item-test.js
+  grunt.registerTask('testClean', function (option) {
+    if (option == undefined) {
+      grunt.fail.fatal('A test file must be specified for testClean. You can also pass "all" to remove all baselines ');
+    }
+    if (option === 'all') {
+      option = '*';
+    }
+    
+    grunt.file.expand([config.regressionTestRoot + '**/' + option + '.test.js']).forEach(function (filepath) {
+      var directory;
+      directory = require('path').dirname(filepath);
+      
+      grunt.log.debug("Attempting to delete: " + directory + "/" + grunt.config.get("phantomcss.all.options.screenshots"));
+      grunt.file.delete(directory + "/" + grunt.config.get("phantomcss.all.options.screenshots"));
+      grunt.log.debug("Attempting to delete: " + directory + "/" + grunt.config.get("phantomcss.all.options.results"));
+      grunt.file.delete(directory + "/" + grunt.config.get("phantomcss.all.options.results"));
+    });
+  });
+
+
+  // test task can accept two params
+  // tests: if this param is blank then a server is spun up and all tests are run
+  //        If a value is passed (i.e. featured-item or *) then featured-item-test.js (or all tests '*') is run without compiling or new server. This is useful when styleguide is already running and you simply want to run tests.
+  // new:   If 'new' is passed in to the second param, then the associated baselines for the selected test file is deleted before running
+  //
+  // grunt test
+  // grunt test:*
+  // grunt test:featured-item:new
+  //grunt.registerTask('test', function (tests, isNew) {
+  //  if (tests == undefined) {
+  //    grunt.config.set('phantomcss.all.src', 'src/sass/**/*.tests.js');
+  //  }
+  //  else {
+  //    if (isNew == 'new') {
+  //      grunt.task.run('testClean:' + tests);
+  //    }
+  //    grunt.config.set('phantomcss.all.src', 'src/sass/**/' + tests + '.tests.js');
+  //  }
+  //  grunt.task.run('phantomcss');
+  //});
 
 };
