b3c9bdeba4d7f2badc3888e514d95d964f8f518e
Evan Lovely <evan@evanlovely.com>
2015-12-15 17:30:28 -0800

    pulling in gulp setup; pulling out grunt setup

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+'use strict';
+var gulp = require('gulp');
+var yaml = require('js-yaml');
+var fs = require('fs');
+var config = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
+var tasks = {
+  'compile': [],
+  'watch': [],
+  'validate': [],
+  'default': []
+};
+
+if (config.browserSync.enabled) {
+  require('p2-theme-core/lib/browser-sync.gulp.js')(gulp, config, tasks);
+}
+
+if (config.js.enabled) {
+  require('p2-theme-core/lib/js.gulp.js')(gulp, config, tasks);
+}
+
+if (config.css.enabled) {
+  require('p2-theme-core/lib/css.gulp.js')(gulp, config, tasks);
+}
+
+if (config.icons.enabled) {
+  require('p2-theme-core/lib/icons.gulp.js')(gulp, config, tasks);
+}
+
+if (config.patternLab.enabled) {
+  require('p2-theme-core/lib/pattern-lab.gulp.js')(gulp, config, tasks);
+}
+
+gulp.task('compile', tasks.compile);
+gulp.task('validate', tasks.validate);
+gulp.task('watch', tasks.watch);
+tasks.default.push('compile');
+tasks.default.push('watch');
+gulp.task('default', tasks.default);
