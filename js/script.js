6e1106f1b5f57b24a0e8f77fb5b2f63b7a62d816
Evan Lovely <evan@evanlovely.com>
2015-12-16 17:22:14 -0800

    setting up babel and eslint

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 (function($, Drupal) {
-  "use strict";
+  'use strict';
 
   // A demo Drupal behavior
   Drupal.behaviors.customBehavior = {
     attach: function(context, settings) {
       //$('body', context).prepend('Demo drupal behavior in js/script.js');
       //console.log('Drupal behavior from js/script.js');
+      let x = 'hi';
+      console.log(x);
     }
   };
   Drupal.behaviors.HighlightJsStartup = {
