f517a90a913aff2a34b8da3530a0221605c1a7c0
Evan Lovely <evan@evanlovely.com>
2016-06-01 23:08:08 -0700

    final tweaks

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
-/*global hljs */
-(function($, Drupal) {
-  'use strict';
-
-  // A demo Drupal behavior
-  Drupal.behaviors.customBehavior = {
-    attach: function(context, settings) {
-      //$('body', context).prepend('Demo drupal behavior in js/script.js');
-      //console.log('Drupal behavior from js/script.js');
-    }
-  };
-  Drupal.behaviors.HighlightJsStartup = {
-    attach: function(context, settings) {
-      //$('body', context).prepend('Demo drupal behavior in js/script.js');
-      hljs.initHighlightingOnLoad();
-      //console.log(hljs.listLanguages());
-    }
-  };
-
-
-})(jQuery, Drupal);
-
+'use strict';
+$(document).ready(function() {
+  $('html').addClass('js');
+});
