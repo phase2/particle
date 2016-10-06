5748aa1d82c8f56056963ee67367e458cc5aca13
Evan Lovely <evan@evanlovely.com>
2016-10-06 13:30:03 -0700

    moving config from yaml to js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,7 @@
 'use strict';
 var gulp = require('gulp');
-var yaml = require('js-yaml');
-var fs = require('fs');
 // `rc` allows all config options to be overridden with CLI flags like `--js.enabled=''` or in `~/.p2-theme-corerc` files, among many others: https://www.npmjs.com/package/rc
-var localConfig = yaml.safeLoad(fs.readFileSync('./gulpconfig.yml', 'utf8'));
-var config = require('rc')('p2-theme-core', localConfig);
+var config = require('rc')('p2-theme-core', require('./gulpconfig.js'));
 var tasks = {
   'compile': [],
   'watch': [],
