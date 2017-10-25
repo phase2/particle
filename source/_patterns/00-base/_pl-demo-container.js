bbde13eb75e45eff758060df02d4f08bb04772b6
rloos289 <rloos289@gmail.com>
2017-10-24 18:32:19 -0700

    Set up all bootstrap buttons

--- source/_patterns/00-base/_pl-demo-container.js
+++ source/_patterns/00-base/_pl-demo-container.js
@@ -1,2 +1,1 @@
 require('../../scss/_config.scss');
-require('~bootstrap/scss/')
--- source/_patterns/01-atoms/button/index.js
+++ source/_patterns/01-atoms/button/index.js
@@ -1,9 +1,11 @@
 // import $ from 'jquery';
 // might be good to force in some jquery here
+import button from 'bootstrap/js/src/button';
 
 require('./button.scss');
 
 export default {
+  button: [button],
   name: 'button_primary',
   enable: () => {},
   disable: () => {},
