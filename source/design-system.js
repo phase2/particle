ecd7a54eb47a5e1d74411d3ea8b6f6c70cdb8e2c
Christopher Bloom <bloomcb@gmail.com>
2017-10-26 09:34:04 -0700

    bootstrap 4 starting ref

--- source/design-system.js
+++ source/design-system.js
@@ -1,11 +1,12 @@
 // import './_patterns/00-base/_config.scss';
 
-// All of bootstrap
-import './scss/_vendor.scss';
 
 // import card from './_patterns/02-molecules/card/card';
 // import homepage from './_patterns/05-pages/homepage';
 import button from './_patterns/01-atoms/button';
+
+// All of bootstrap
+// import './scss/_vendor.scss';
 
 const components = [];
 
