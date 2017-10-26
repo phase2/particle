5cc8d5516e96e603c2d9af3c4549ff28bcd32ede
Christopher Bloom <bloomcb@gmail.com>
2017-10-25 21:40:33 -0700

    progress toward bootstrap integration

--- source/design-system.js
+++ source/design-system.js
@@ -1,15 +1,16 @@
-import card from './_patterns/02-molecules/card/card';
-import homepage from './_patterns/05-pages/homepage';
-import button from './_patterns/01-atoms/button';
+import './scss/_config.scss';
 
-require('./scss/_config.scss');
+// All of bootstrap
+// import './scss/_vendor.scss';
+
+// import card from './_patterns/02-molecules/card/card';
+// import homepage from './_patterns/05-pages/homepage';
+import button from './_patterns/01-atoms/button';
 
 const components = [];
 
-components.push(card);
-components.push(homepage);
+// components.push(card);
+// components.push(homepage);
 components.push(button);
 
 export default components;
-
-console.log('hello from design-system.js');
