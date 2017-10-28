644a3a2d6dcd2fcb34177b5baf60e319ba6f662b
Christopher Bloom <bloomcb@gmail.com>
2017-10-27 20:41:27 -0700

    Documentation

--- source/design-system.js
+++ source/design-system.js
@@ -2,8 +2,6 @@
  * The kitchen sink of all design components
  */
 
-import GLOBAL_CONSTANT from './_patterns/00-base';
-
 import button from './_patterns/01-atoms/button';
 import card from './_patterns/02-molecules/card';
 // import homepage from './_patterns/05-pages/homepage';
@@ -14,8 +12,5 @@ const components = [];
 components.push(button);
 components.push(card);
 // components.push(homepage);
-
-
-console.log(GLOBAL_CONSTANT);
 
 export default components;
