70f18a8838756795b0f4a5dadf5886dba314a84a
Christopher Bloom <bloomcb@gmail.com>
2017-10-29 11:55:19 -0700

    rgba to luminance twig filters because why not

---
+++ source/_twig-components/filters/rgba_string.filter.php
@@ -0,0 +1,14 @@
+<?php
+
+$filter = new Twig_SimpleFilter('rgba_string', function ($string) {
+  $rgba = trim(str_replace(' ', '', $string));
+  if (stripos($rgba, 'rgba') !== false) {
+    $res = sscanf($rgba, "rgba(%d, %d, %d, %f)");
+  }
+  else {
+    $res = sscanf($rgba, "rgb(%d, %d, %d)");
+    $res[] = 1;
+  }
+
+  return array_combine(array('r', 'g', 'b', 'a'), $res);
+});
