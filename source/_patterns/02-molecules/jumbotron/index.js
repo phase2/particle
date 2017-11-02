738de0d6f9e2e88d05cdc192006796d65a7cb791
J. Ryan Conklin <jryanconklin@gmail.com>
2017-11-02 14:52:56 -0700

    Removes holder from jumbotron

--- source/_patterns/02-molecules/jumbotron/index.js
+++ source/_patterns/02-molecules/jumbotron/index.js
@@ -13,12 +13,7 @@ import './_jumbotron.scss';
 export default {
   name: 'jumbotron',
   deps: [base, image, button],
-  enable($context, { enableHolder }) {
-    // Holder is only provided to PL, not Drupal. Like jQuery it's an IIFE
-    // We can only conditionally import libraries using `require()`
-    if (enableHolder) {
-      require('holderjs'); // eslint-disable-line global-require
-    }
+  enable($context) {
   },
   disable() {},
 };
