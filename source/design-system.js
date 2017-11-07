e422a0e9649ce072a0925fc665e56b8a3b78fefb
J. Ryan Conklin <jryanconklin@gmail.com>
2017-11-06 16:52:08 -0800

    PLP10: Update Breadcrumbs Demo

--- source/design-system.js
+++ source/design-system.js
@@ -2,29 +2,29 @@
  * The kitchen sink of all design components
  */
 
+import breadcrumb from 'atoms/breadcrumb';
 import button from 'atoms/button';
 import image from 'atoms/image';
 import listItem from 'atoms/list-item';
 import card from 'molecules/card';
-import breadcrumb from 'molecules/breadcrumb';
 import jumbotron from 'molecules/jumbotron';
 
 // Export as a plain ol' object if we need
 export default {
+  breadcrumb,
   button,
   image,
   listItem,
   card,
-  breadcrumb,
   jumbotron,
 };
 
 // Provide each individually
 export {
+  breadcrumb,
   button,
   image,
   listItem,
   card,
-  breadcrumb,
   jumbotron,
 };
