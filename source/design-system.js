b329729ceaf5c0c975b10cdc6d7ab86fc4da6dc5
tjheffner <tannerjheffner@gmail.com>
2017-11-08 16:12:30 -0800

    actually fix merge conflicts

--- source/design-system.js
+++ source/design-system.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import vueComponents from './vue-app';
-import card from './_patterns/02-molecules/card/card';
-import homepage from './_patterns/05-pages/homepage';
-
-require('./scss/_config.scss');
-=======
 /**
  * The kitchen sink of all design components
  */
@@ -16,6 +9,7 @@ import icon from 'atoms/icon';
 import listItem from 'atoms/list-item';
 import card from 'molecules/card';
 import jumbotron from 'molecules/jumbotron';
+import vueComponents from './vue-app';
 
 // Plain ol' object in ES6 object shorthand syntax
 const designSystem = {
@@ -26,23 +20,14 @@ const designSystem = {
   listItem,
   card,
   jumbotron,
+  vueComponents,
 };
->>>>>>> c1bcf4adfa84a744c109f4e749840301cc93c254
 
 // Provide each individually, allows for e.g.
 // import { card } from './design-system';
 export { designSystem };
 
-<<<<<<< HEAD
-components.push(card);
-components.push(homepage);
-
-export { vueComponents };
-export default components;
-
 console.log('hello from design-system.js');
-=======
 // Default export, allows for e.g.
 // import designSystem from './design-system';
 export default designSystem;
->>>>>>> c1bcf4adfa84a744c109f4e749840301cc93c254
