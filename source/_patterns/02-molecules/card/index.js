40fc2d2d4d5481b7c92181f577614645b2006532
Christopher Bloom <bloomcb@gmail.com>
2017-11-10 16:04:59 -0800

    holderjs logic should be in the image component, not card

--- source/_patterns/02-molecules/card/index.js
+++ source/_patterns/02-molecules/card/index.js
@@ -14,12 +14,6 @@ export const name = 'card';
 
 export function disable() {}
 
-export function enable($context, { enableHolder }) {
-  // Holder is only provided to PL, not Drupal. Like jQuery it's an IIFE
-  // We can only conditionally import libraries using `require()`
-  if (enableHolder) {
-    require('holderjs'); // eslint-disable-line global-require
-  }
-}
+export function enable() {}
 
 export default enable;
