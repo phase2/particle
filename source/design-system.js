53f282f0a6051fa4459932a8d8eaf63f38cc391f
J. Ryan Conklin <jryanconklin@gmail.com>
2017-11-15 13:20:07 -0800

    PLP-30: Move branding to Atom

--- source/design-system.js
+++ source/design-system.js
@@ -3,12 +3,12 @@
  * The kitchen sink of all design components
  */
 
+import * as branding from 'atoms/branding';
 import * as breadcrumb from 'atoms/breadcrumb';
 import * as button from 'atoms/button';
 import * as image from 'atoms/image';
 import * as icon from 'atoms/icon';
 import * as listItem from 'atoms/list-item';
-import * as branding from 'molecules/branding';
 import * as card from 'molecules/card';
 import * as jumbotron from 'molecules/jumbotron';
 
