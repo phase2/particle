6b37b617bfb14d7e88cb1129269e0a65995e53a2
J. Ryan Conklin <jryanconklin@gmail.com>
2017-11-15 16:38:47 -0800

    PLP-29: Scaffold from PLP-30

--- source/design-system.js
+++ source/design-system.js
@@ -6,6 +6,7 @@
 import * as branding from 'atoms/branding';
 import * as breadcrumb from 'atoms/breadcrumb';
 import * as button from 'atoms/button';
+import * as grid from 'atoms/grid';
 import * as image from 'atoms/image';
 import * as icon from 'atoms/icon';
 import * as listItem from 'atoms/list-item';
@@ -13,12 +14,13 @@ import * as card from 'molecules/card';
 import * as jumbotron from 'molecules/jumbotron';
 
 export default {
+  branding,
   breadcrumb,
   button,
+  grid,
   image,
   icon,
   listItem,
-  branding,
   card,
   jumbotron,
 };
