d8237340c77548314ef1be5b45fbddaa6be5a2cc
Christopher Bloom <bloomcb@gmail.com>
2017-12-31 16:56:24 -0800

    Mock popper to pass bootstrap, dropdown tests

--- source/_patterns/01-atoms/dropdown/__tests__/dropdown.test.js
+++ source/_patterns/01-atoms/dropdown/__tests__/dropdown.test.js
@@ -2,21 +2,21 @@ const $ = require('jquery');
 const dropdown = require('../');
 
 const dropHTML = `
-<div class="dropdown">
-  <button type="button" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="dropdownMenuButton">Dropdown button</button>
-  <div class="dropdown-menu " aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item " href="#">Action</a>
-    <a class="dropdown-item " href="#">Another Action</a>
-    <a class="dropdown-item " href="#">Something else here</a>
+  <div class="dropdown">
+    <button type="button" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="dropdownMenuButton">Dropdown button</button>
+    <div class="dropdown-menu " aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item " href="#">Action</a>
+      <a class="dropdown-item " href="#">Another Action</a>
+      <a class="dropdown-item " href="#">Something else here</a>
+    </div>
   </div>
-</div>
 `;
 
 test('dropdown component is registered', () => {
   expect(dropdown.name).toBe('dropdown');
 });
 
-test('dropdown menu on button click', () => {
+test('dropdown menu starts out hidden', () => {
   document.body.innerHTML = dropHTML;
   dropdown.enable($(document));
 
@@ -26,11 +26,14 @@ test('dropdown menu on button click', () => {
   expect($('.dropdown-toggle').attr('aria-expanded')).toBe('false');
 });
 
-test('dropdown menu starts out hidden', () => {
+test('dropdown menu shows after clicking button', () => {
   document.body.innerHTML = dropHTML;
   dropdown.enable($(document));
 
   $('#dropdownMenuButton').click();
+
+  // `show` class should now be on dropdowns
   expect($('.dropdown, .dropdown-menu').hasClass('show')).toBe(true);
+  // attributes are strings, not booleans
   expect($('.dropdown-toggle').attr('aria-expanded')).toBe('true');
 });
---
+++ tools/tests/unit/__mocks__/popper.js.js
@@ -0,0 +1,17 @@
+/**
+ * Popper.js is required by Bootstrap and kersplodes Jest jsdoc.
+ *
+ * This mocks just enough to get basics to pass.
+ */
+import PopperJs from 'popper.js';
+
+export default class Popper {
+  static placements = PopperJs.placements;
+
+  constructor() {
+    return {
+      destroy: () => {},
+      scheduleUpdate: () => {}
+    };
+  }
+}
