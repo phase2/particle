49ac7057be28669db84845581a17752304155161
Christopher Bloom <bloomcb@gmail.com>
2018-01-02 10:13:49 -0800

    Protons dummy var note

--- source/_patterns/00-protons/__tests__/protons.test.js
+++ source/_patterns/00-protons/__tests__/protons.test.js
@@ -1,3 +1,10 @@
+/**
+ * Tests for protons
+ *
+ * The tests below are only a very minimal start to verify that we can access a global. You
+ * probably do not actually need a `blerp` value in your code. If you do, please let us know!
+ */
+
 import protons from '../';
 
 test('GLOBAL_CONSTANT of `blerp` is available', () => {
