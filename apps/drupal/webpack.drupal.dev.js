0f796621b8b57e60fd9e2187cde5c866438a19b2
Christopher Bloom <bloomcb@gmail.com>
2018-03-04 12:04:11 -0800

    Drupal dev settings

---
+++ apps/drupal/webpack.drupal.dev.js
@@ -0,0 +1,24 @@
+/**
+ * Drupal-specific webpack config.
+ * This is merged over top of webpack.particle.dev.js
+ */
+
+// Library Imports
+const merge = require('webpack-merge');
+
+// Custom Imports
+// Import shared PROD settings because webpack.particle.prod extracts styles to its own standalone
+// CSS files. So drupal dev uses shared prod settings, in dev mode to rapidly build all assets.
+const particle = require('../../webpack.particle.prod.js');
+const drupal = require('./webpack.drupal.shared');
+
+// Webpack Entry Points
+const dev = {
+  mode: 'development',
+  stats: {
+    children: false,
+    entrypoints: false,
+  },
+};
+
+module.exports = merge(particle, drupal, dev);
--- apps/drupal/webpack.drupal.prod.js
+++ apps/drupal/webpack.drupal.prod.js
@@ -4,30 +4,14 @@
  */
 
 // Library Imports
-const path = require('path');
 const merge = require('webpack-merge');
-const webpack = require('webpack');
 
 // Custom Imports
-const shared = require('../../webpack.particle.prod.js');
+const particle = require('../../webpack.particle.prod.js');
+const drupal = require('./webpack.drupal.shared');
 
-const drupal = {
+const prod = {
   mode: 'production',
-  entry: {
-    'app-drupal': [
-      path.resolve(__dirname, 'index.js'),
-    ],
-  },
-  // These will be explicitly be provided OUTSIDE the bundle via a <script> tag in the HTML
-  externals: {
-    jquery: 'jQuery',
-    lodash: '_',
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      BUILD_TARGET: JSON.stringify('drupal'),
-    }),
-  ],
 };
 
-module.exports = merge(shared, drupal);
+module.exports = merge(particle, drupal, prod);
