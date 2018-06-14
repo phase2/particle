14eb351668cda7fd4617cf3146357a32548fa914
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-06-14 15:23:29 -0700

    Create initial implementation using SVG Sprites

---
+++ source/_patterns/01-atoms/svgicon/index.js
@@ -0,0 +1,20 @@
+/**
+ * svgicon
+ */
+
+// Module dependencies
+import 'protons';
+
+// Module styles
+import './_svgicon.scss';
+
+// Module template
+import './_svgicon.twig';
+
+export const name = 'svgicon';
+
+export function disable() {}
+
+export function enable() {}
+
+export default enable;
