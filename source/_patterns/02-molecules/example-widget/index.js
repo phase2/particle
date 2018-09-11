b4c443c376c5d8f116850b8919a664597a63958c
Frederick Engelhardt <frederickengelhardt@Fredericks-MacBook-Pro.local>
2018-09-11 10:48:47 -0700

    now puts the Vue vanilla into it's section and Vuex has it's own area as well. Renamed variables to match their types

--- source/_patterns/02-molecules/example-widget/index.js
+++ source/_patterns/02-molecules/example-widget/index.js
@@ -13,11 +13,15 @@ import './_vue-facet-table-vuex.twig';
 // Module styles
 import './_example-widget.scss';
 
-import FacetTable from './src/vuex-example'; // mount only if dom found
+import FacetTable from './src/vue-example'; // mount only if dom found
+import FacetTableVx from './src/vuex-example'; // mount only if dom found
 
 // Render Vue elements as soon as possible
+if (document.getElementById('vue-facet-table-vue')) {
+  new FacetTable().$mount('#vue-facet-table-vue');
+}
 if (document.getElementById('vue-facet-table-vuex')) {
-  new FacetTable().$mount('#vue-facet-table-vuex');
+  new FacetTableVx().$mount('#vue-facet-table-vuex');
 }
 
 export const name = 'example-widget';
