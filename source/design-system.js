02f848c54b6fa86590ea2015d1aa37410762576c
Christopher Bloom <bloomcb@gmail.com>
2018-09-25 21:54:43 -0700

    Webpack now finds components dynamically via require.context()

--- source/design-system.js
+++ source/design-system.js
@@ -1,58 +1,80 @@
 /**
  * Design System
- * The kitchen sink of all design components
+ * The kitchen sink of all design components. There is a little bit of "magic"
+ * below, so be sure to read each section.
  */
-import * as alert from 'atoms/alert';
-import * as branding from 'atoms/branding';
-import * as breadcrumb from 'atoms/breadcrumb';
-import * as button from 'atoms/button';
-import * as dropdown from 'atoms/dropdown';
-import * as grid from 'atoms/grid';
-import * as image from 'atoms/image';
-import * as svgicon from 'atoms/svgicon';
-import * as listGroup from 'atoms/list-group';
-import * as nav from 'molecules/nav';
-import * as card from 'molecules/card';
-import * as carousel from 'molecules/carousel';
-import * as jumbotron from 'molecules/jumbotron';
-import * as pagination from 'molecules/pagination';
-import * as vueWidgets from 'molecules/vue-widget';
-import * as accordion from 'organisms/accordion';
-import * as article from 'organisms/article';
-import * as footer from 'organisms/footer';
-import * as navbar from 'organisms/navbar';
-import * as basicPage from 'templates/basic-page';
 
-const molecules = require.context(
-  './_patterns/02-molecules',
-  true,
-  /index.js$/
+import { basename, dirname } from 'path';
+
+const components = {};
+
+/**
+ * Accepts the result of require.context() to add it all to our great big
+ * components object with keys that are the folder names of components, ie
+ *
+ *   01-atoms/vue-widget/index.js
+ *
+ * results in:
+ *
+ *   context['vue-widget'] = {name: 'vue-widget', enable() {}, disable() {}}
+ *
+ * @param context
+ */
+function importAll(context) {
+  context.keys().forEach(componentPath => {
+    const componentName = basename(dirname(componentPath));
+    components[componentName] = context(componentPath);
+  });
+}
+
+/**
+ * Dynamically discover all root patterns using Webpack's require.context().
+ * This attempts to find ONLY the first index.js file within a component folder.
+ * Important: the regex must be "statically analyzable", meaning we cannot set
+ * the regex to a variable. (https://github.com/webpack/webpack/issues/4772).
+ *
+ * @TODO: A cleaner regex to get only the first level index.js, no deeper
+ */
+// Atoms
+importAll(
+  require.context(
+    './_patterns/01-atoms',
+    true,
+    /^(?!.*(demo|src)).*index\.js$/ //
+  )
 );
-console.log(molecules);
-molecules.keys().forEach(filename => console.log(filename));
+// Molecules
+importAll(
+  require.context(
+    './_patterns/02-molecules',
+    true,
+    /^(?!.*(demo|src)).*index\.js$/
+  )
+);
+// Organisms
+importAll(
+  require.context(
+    './_patterns/03-organisms',
+    true,
+    /^(?!.*(demo|src)).*index\.js$/
+  )
+);
+// Templates (skipping for design system)
+// importAll(
+//   require.context(
+//     './_patterns/04-templates',
+//     true,
+//     /^(?!.*(demo|src)).*index\.js$/
+//   )
+// );
+// Pages (skipping for design system)
+// importAll(
+//   require.context(
+//     './_patterns/05-pages',
+//     true, /^(?!.*(demo|src)).*index\.js$/
+//   )
+// );
 
-const components = {
-  alert,
-  branding,
-  breadcrumb,
-  button,
-  dropdown,
-  grid,
-  image,
-  svgicon,
-  listGroup,
-  nav,
-  card,
-  carousel,
-  jumbotron,
-  pagination,
-  vueWidgets,
-  accordion,
-  article,
-  footer,
-  navbar,
-  basicPage,
-};
 /**
  * Default export of object containing all components
  */
