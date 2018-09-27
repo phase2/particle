d63887b2080171f4bac0692e95cac8d24201da15
Christopher Bloom <bloomcb@gmail.com>
2018-09-27 11:13:36 -0700

    Starting integration of settings between component and drupal.

--- source/_patterns/02-molecules/carousel/index.js
+++ source/_patterns/02-molecules/carousel/index.js
@@ -18,11 +18,34 @@ import './_carousel.scss';
 
 export const name = 'carousel';
 
-export function disable() {}
+export const defaults = {
+  interval: 3000,
+};
 
-export function enable($context) {
+/**
+ * Components may need to run clean-up tasks if they are removed from DOM.
+ *
+ * @param {jQuery} $context - A piece of DOM
+ * @param {Object} settings - Pertinent settings
+ */
+// eslint-disable-next-line no-unused-vars
+export function disable($context, settings) {}
+
+/**
+ * Each component has a chance to run when its enable function is called. It is
+ * given a piece of DOM ($context) and a settings object. We destructure our
+ * component key off the settings object and provide an empty object fallback.
+ * Incoming settings override default settings via Object.assign().
+ *
+ * @param {jQuery} $context - A piece of DOM
+ * @param {Object} settings - Settings object
+ */
+export function enable($context, { carousel = {} }) {
+  const settings = Object.assign(defaults, carousel);
+
+  // Initialize the carousel with overridable settings
   $('.carousel', $context).carousel({
-    interval: 3000,
+    interval: settings.interval,
   });
 }
 
