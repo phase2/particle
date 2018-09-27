36bc0b27ac9ae5c39fd6fa261a36185a695541cf
Christopher Bloom <bloomcb@gmail.com>
2018-09-27 11:56:56 -0700

    Update npm run new with pattern better showing what could be done.

--- tools/new-component/templates/pattern.js
+++ tools/new-component/templates/pattern.js
@@ -15,16 +15,39 @@ import './_<%= name %>.twig';
 
 export const name = '<%= camelCaseName %>';
 
-export function disable() {}
+export const defaults = {
+  dummyClass: 'js-<%= camelCaseName %>-exists',
+};
 
-export function enable($context) {
+/**
+ * Components may need to run clean-up tasks if they are removed from DOM.
+ *
+ * @param {jQuery} $context - A piece of DOM
+ * @param {Object} settings - Pertinent settings
+ */
+// eslint-disable-next-line no-unused-vars
+export function disable($context, settings) {}
+
+/**
+ * Each component has a chance to run when its enable function is called. It is
+ * given a piece of DOM ($context) and a settings object. We destructure our
+ * component key off the settings object and provide an empty object fallback.
+ * Incoming settings override default settings via Object.assign().
+ *
+ * @param {jQuery} $context - A piece of DOM
+ * @param {Object} settings - Settings object
+ */
+export function enable($context, { <%= camelCaseName %> = {} }) {
+  // Find our component within the DOM
   const $<%= camelCaseName %> = $('.<%= camelCaseName %>', $context);
   // Bail if component does not exist
   if (!$<%= camelCaseName %>.length) {
     return;
   }
-  // Only do stuff to component if it exists
-  $<%= camelCaseName %>.addClass('js-exists'); // Please remove/change this
+  // Merge defaults with incoming settings
+  const settings = Object.assign(defaults, <%= camelCaseName %>);
+  // An example of what could be done with this component
+  $<%= camelCaseName %>.addClass(settings.dummyClass);
 }
 
 export default enable;
