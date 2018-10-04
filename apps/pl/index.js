54f280cc65bd92a400d5be397b5ccf1bc95552d2
Christopher Bloom <bloomcb@gmail.com>
2018-10-03 18:23:54 -0700

    Bring back prism

--- apps/pl/index.js
+++ apps/pl/index.js
@@ -9,13 +9,13 @@ import 'atoms/grid';
 
 // Prism highlighting in PL.
 // Prism is kind of dumb and just operates globally.
-// import 'prismjs/components/prism-core.min';
-// import 'prismjs/components/prism-clike.min';
-// import 'prismjs/components/prism-javascript.min';
-// import 'prismjs/components/prism-twig.min';
-// import 'prismjs/components/prism-css.min';
-// import 'prismjs/components/prism-scss.min';
-// import 'prismjs/components/prism-markup.min';
+import 'prismjs/components/prism-core.min';
+import 'prismjs/components/prism-clike.min';
+import 'prismjs/components/prism-javascript.min';
+import 'prismjs/components/prism-twig.min';
+import 'prismjs/components/prism-css.min';
+import 'prismjs/components/prism-scss.min';
+import 'prismjs/components/prism-markup.min';
 
 // Full design system. May dupe the above, but Webpack don't care.
 import { enableAllComponents } from '../../source/design-system';
