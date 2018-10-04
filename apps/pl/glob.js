947fb0e909f9092ada8633a6445b0aa1c8082897
Christopher Bloom <bloomcb@gmail.com>
2018-10-03 18:35:24 -0700

    Cleanup

--- apps/pl/glob.js
+++ apps/pl/glob.js
@@ -9,14 +9,6 @@ import importAll from '../../tools/webpack/import-all';
 import '../../source/_meta/_00-head.twig';
 import '../../source/_meta/_01-foot.twig';
 
-// Iterate on a context and require
-// function importAll(context) {
-//   context.keys().forEach(componentPath => {
-//     // console.log(componentPath);
-//     context(componentPath);
-//   });
-// }
-
 // Watch all demo folders in source
 const demoSystem = importAll(
   require.context('../../source/_patterns', true, /demo$/)
