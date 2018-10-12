4edf3981aedcb851106f1dbe55cd61bb25dd07ec
Abel Trotter <a_trotter0@hotmail.com>
2018-10-12 14:27:39 -0700

    Use const instead of let for paths variable

--- tools/pl-paths.js
+++ tools/pl-paths.js
@@ -2,7 +2,7 @@
 
 const plData = require('../dist/pl/styleguide/data/patternlab-data.json');
 const particles = Object.keys(plData.patternPaths);
-let paths = [];
+const paths = [];
 
 particles.forEach(particle => {
   const urls = Object.values(plData.patternPaths[particle]);
