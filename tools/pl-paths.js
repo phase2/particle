669ec1665e574bcd4221a404bb03839fee0561b4
Abel Trotter <a_trotter0@hotmail.com>
2018-10-12 14:20:21 -0700

    Remove hardcoded array values, use spread instead of concat

--- tools/pl-paths.js
+++ tools/pl-paths.js
@@ -1,18 +1,11 @@
-//run by typing : node tools/pl-paths.js
+//run with : node tools/pl-paths.js
 
 const plData = require('../dist/pl/styleguide/data/patternlab-data.json');
-const particles = [
-  'protons',
-  'atoms',
-  'molecules',
-  'organisms',
-  'templates',
-  'pages',
-];
+const particles = Object.keys(plData.patternPaths);
 let paths = [];
 
 particles.forEach(particle => {
   const urls = Object.values(plData.patternPaths[particle]);
-  paths = paths.concat(urls);
+  paths.push(...urls);
 });
 console.log(paths);
