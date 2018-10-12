b6f296734226b396b706903a1ac5c283e85ebd45
Abel Trotter <a_trotter0@hotmail.com>
2018-10-12 16:44:11 -0700

    Change map variable for readability

--- tools/pl-paths.js
+++ tools/pl-paths.js
@@ -2,13 +2,15 @@ const {
   patternPaths,
 } = require('../dist/pl/styleguide/data/patternlab-data.json');
 
-// Returns an array of urls for the particles that exist
-// in a particle project.
+// Returns an array of urls for the patterns that exist
+// in a pattern lab project.
 // Example output:
 // [
 //   '00-protons-demo-borders',
 //   '00-protons-demo-breakpoints',
 // ]
 module.exports = [].concat(
-  ...Object.keys(patternPaths).map(url => Object.values(patternPaths[url]))
+  ...Object.keys(patternPaths).map(pattern =>
+    Object.values(patternPaths[pattern])
+  )
 );
