fabc8291f5b803469f703eba79455d99d73eac41
Christopher Bloom <bloomcb@gmail.com>
2018-10-16 11:09:15 -0700

    pa11y update (#431)
    * Update pa11y to automatically read from PL data to derive all urls. Use pa11y-ci instead of pa11y library.
    
    * object shorthand is cool
    
    * Components Error Handling and Page (#427)
    
    * Set page var to handle components with error page
    
    * Update spelling error
    
    * Update variable name to prevent collisions
    
    * Update var name in page.html docblock
    
    * Update font family and move template to misc
    
    * Add |t to strings
    
    * Multi exports from pl-paths to allow just the base paths as well as the full html paths
    
    * Cleaner values map

---
+++ tools/pl-paths.js
@@ -0,0 +1,29 @@
+/**
+ * Extract all the Pattern Lab paths out of built PL assets
+ */
+
+const {
+  patternPaths,
+} = require('../dist/pl/styleguide/data/patternlab-data.json');
+
+// Shape of data:
+//
+// patternPaths: {
+//   protons: {
+//     borders: '00-protons-demo-borders',
+//     tables: '00-protons-demo-tables',
+//     // ...
+//   },
+//   atoms: {},
+//   molecules: {},
+//   // ...
+// }
+
+const paths = [].concat(
+  ...Object.values(patternPaths).map(atomic => Object.values(atomic))
+);
+
+module.exports = {
+  paths,
+  htmlPaths: paths.map(path => `patterns/${path}/${path}.html`),
+};
