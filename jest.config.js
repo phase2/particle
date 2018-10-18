18eba79641e083ea8500c26478fd2bcb70c2b637
Christopher Bloom <bloomcb@gmail.com>
2018-10-18 09:53:52 -0700

    Remove webpack aliases

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,21 @@
-/* eslint max-len: 0 */
 module.exports = {
   verbose: true,
   testURL: 'http://localhost/',
   setupFiles: ['<rootDir>/tools/tests/unit/setupJest.js'],
+  moduleFileExtensions: ['js', 'json', 'vue'],
   moduleNameMapper: {
     // Jest doesn't care about styles, twig, images, fonts, etc
     '\\.(twig|md|yml|yaml|css|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/tools/tests/unit/__mocks__/fileMock.js',
+    '^protons[/]?(.*)': '<rootDir>/source/_patterns/00-protons/$1',
+    '^atoms/(.+)': '<rootDir>/source/_patterns/01-atoms/$1',
+    '^molecules/(.+)': '<rootDir>/source/_patterns/02-molecules/$1',
+    '^organisms/(.+)': '<rootDir>/source/_patterns/03-organisms/$1',
+    '^templates/(.+)': '<rootDir>/source/_patterns/04-templates/$1',
+    '^pages/(.+)': '<rootDir>/source/_patterns/05-pages/$1',
   },
   transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor',
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.vue$': 'vue-jest',
   },
 };
--- source/_patterns/02-molecules/vue-widget/__tests__/vue-cards.spec.js
+++ source/_patterns/02-molecules/vue-widget/__tests__/vue-cards.spec.js
@@ -1,7 +1,8 @@
 import { render, Simulate } from 'vue-testing-library';
-import card from '../src/vue-cards/components/card.vue';
-import cards from '../src/vue-cards/components/cards.vue';
-import banner from '../src/vue-cards/components/banner.vue';
+
+import card from 'molecules/vue-widget/src/vue-cards/components/card.vue';
+import cards from 'molecules/vue-widget/src/vue-cards/components/cards.vue';
+import banner from 'molecules/vue-widget/src/vue-cards/components/banner.vue';
 
 describe('card.vue', () => {
   it('renders component with supplied props', () => {
