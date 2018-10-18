f63ee0738a1d3b3117040dd2e6cf427de2579b5a
Christopher Bloom <bloomcb@gmail.com>
2018-10-17 22:23:15 -0700

    Unify all jest regex aliases

--- jest.config.js
+++ jest.config.js
@@ -13,11 +13,11 @@ module.exports = {
     // Protons use a slightly different pattern because it's the only source pattern directory that
     // has a base-level index.js (this is how all components ensure access to base-level styles & variables).
     '^protons[/]?(.*)': '<rootDir>/source/default/_patterns/00-protons/$1',
-    '^atoms/(.+)': '<rootDir>/source/default/_patterns/01-atoms/$1',
-    '^molecules/(.+)': '<rootDir>/source/default/_patterns/02-molecules/$1',
-    '^organisms/(.+)': '<rootDir>/source/default/_patterns/03-organisms/$1',
-    '^templates/(.+)': '<rootDir>/source/default/_patterns/04-templates/$1',
-    '^pages/(.+)': '<rootDir>/source/default/_patterns/05-pages/$1',
+    '^atoms[/]?(.*)': '<rootDir>/source/default/_patterns/01-atoms/$1',
+    '^molecules[/]?(.*)': '<rootDir>/source/default/_patterns/02-molecules/$1',
+    '^organisms[/]?(.*)': '<rootDir>/source/default/_patterns/03-organisms/$1',
+    '^templates[/]?(.*)': '<rootDir>/source/default/_patterns/04-templates/$1',
+    '^pages[/]?(.*)': '<rootDir>/source/default/_patterns/05-pages/$1',
   },
   transform: {
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
