98300ff9a776165708571641f2052e9204a27a89
Christopher Bloom <bloomcb@gmail.com>
2018-10-19 08:34:27 -0700

    vue-jest upgrade (#436)
    * babel now understands webpack aliases
    
    * Remove webpack aliases

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,12 @@
-/* eslint max-len: 0 */
 module.exports = {
   verbose: true,
   testURL: 'http://localhost/',
   setupFiles: ['<rootDir>/tools/tests/unit/setupJest.js'],
+  moduleFileExtensions: ['js', 'json', 'vue'],
   moduleNameMapper: {
     // Jest doesn't care about styles, twig, images, fonts, etc
     '\\.(twig|md|yml|yaml|css|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/tools/tests/unit/__mocks__/fileMock.js',
-
-    // Webpack aliases. This is a pain, but Babel does NOT understand Webpack resolved aliases
-    // @TODO: Look into helpers for this (e.g. https://github.com/mwolson/jest-webpack-alias)
-    // Protons use a slightly different pattern because it's the only source pattern directory that
-    // has a base-level index.js (this is how all components ensure access to base-level styles & variables).
     '^protons[/]?(.*)': '<rootDir>/source/_patterns/00-protons/$1',
     '^atoms/(.+)': '<rootDir>/source/_patterns/01-atoms/$1',
     '^molecules/(.+)': '<rootDir>/source/_patterns/02-molecules/$1',
@@ -20,7 +15,7 @@ module.exports = {
     '^pages/(.+)': '<rootDir>/source/_patterns/05-pages/$1',
   },
   transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor',
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.vue$': 'vue-jest',
   },
 };
