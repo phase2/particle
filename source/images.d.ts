440fc89658f1554b2b060edfb167a724f0d9d729
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-10-23 21:50:40 -0700

    Add image module type declarations

---
+++ source/images.d.ts
@@ -0,0 +1,13 @@
+/* TypeScript requires declaring files which are imported to
+ * have corresponding module declarations. For base image and file
+ * assets we can have a basic module declaration format as this
+ * is handled by Webpack.
+ */
+
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
