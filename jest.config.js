e636175aae1a636be8902168d2ba530723514f19
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2018-10-24 22:04:37 -0700

    Alter vue-jest TypeScript to use default config

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   verbose: true,
   testURL: 'http://localhost/',
   setupFiles: ['<rootDir>/tools/tests/unit/setupJest.js'],
-  moduleFileExtensions: ['js', 'json', 'vue'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx', 'vue'],
   moduleNameMapper: {
     // Jest doesn't care about styles, twig, images, fonts, etc
     '\\.(twig|md|yml|yaml|css|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
@@ -14,5 +14,12 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
     '^.+\\.vue$': 'vue-jest',
+  },
+  globals: {
+    'vue-jest': {
+      // Use default config for vue-jest since our setup compiles
+      // to ESNext rather than ES5 which vue-jest needs
+      tsConfig: false,
+    },
   },
 };
