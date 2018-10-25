e6bca17acbd4d000ef8c5d5548d596f747f4d480
Jeff Wax <jeff@jeffwax.com>
2018-10-25 15:17:48 -0700

    Add usage of scssvariables.

--- source/default/_patterns/00-protons/index.js
+++ source/default/_patterns/00-protons/index.js
@@ -1,9 +1,16 @@
 /**
- * Base css generation and global js logic
+ * Base css generation and global js logic.
  */
 
 import './_base.scss';
 
+// Import Pattern Lab-generated SCSS and Bootstrap variables for global usage.
+import scssVariables from "../../_data/scssVariables.json";
+
+// Exported variables and constants can be used by importing 'protons' to a
+// container variable, then accessing.
 export default {
   GLOBAL_CONSTANT: 'blerp',
+  GLOBAL_BREAKPOINTS: scssVariables.scssBreakpoints,
+  GLOBAL_COLORS: scssVariables.scssColors,
 };
