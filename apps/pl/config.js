0dddb0b00c26e395818532c029de209cd89c7077
Christopher Bloom <bloomcb@gmail.com>
2018-10-29 16:49:01 -0600

    Update pl config

--- apps/pl/config.js
+++ apps/pl/config.js
@@ -1,20 +1,36 @@
 /**
- * App config
+ * Particle app conventions
  */
 
 const path = require('path');
 
-const config = {
-  APP_NAME: 'app-pl',
-  APP_PATH: path.resolve(__dirname),
-  APP_DESIGN_SYSTEM: path.resolve(__dirname, '../../source/default'),
-  // Namespace formatting
-  namespaces: {
-    config: path.resolve(__dirname, 'pattern-lab/config/config.yml'),
-    atKey: 'plugins.twigNamespaces.namespaces',
-    transform: folderPath =>
-      path.relative(path.join(config.APP_PATH, 'pattern-lab'), folderPath),
-  },
+const { PATH_DIST, ASSETS_BUNDLE_FOLDER } = require('../../config');
+
+// Used as folder name within PATH_DIST, does not have to be folder name of app
+const APP_NAME = 'app-pl';
+// Full path to app
+const APP_PATH = path.resolve(__dirname);
+// Full path to design system used in this app
+const APP_DESIGN_SYSTEM = path.resolve(__dirname, '../../source/default');
+// Where should this app compile to, e.g. dist/app-drupal/assets/
+const APP_DIST = path.join(PATH_DIST, APP_NAME, ASSETS_BUNDLE_FOLDER);
+
+// Namespace formatting, required by the namespaces module
+const namespaces = {
+  // The yml file that will be modified
+  config: path.resolve(__dirname, 'pattern-lab/config/config.yml'),
+  // The key within the file where the namespaces will be written
+  atKey: 'plugins.twigNamespaces.namespaces',
+  // The namespaces module runs a transform function, providing to it the path
+  // to the pattern within source/
+  transform: folderPath =>
+    path.relative(path.join(APP_PATH, 'pattern-lab'), folderPath),
 };
 
-module.exports = config;
+module.exports = {
+  APP_NAME,
+  APP_PATH,
+  APP_DESIGN_SYSTEM,
+  APP_DIST,
+  namespaces,
+};
