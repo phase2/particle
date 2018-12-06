dc652b5483786ccd9b3aba3f48a6157161e1f47b
Jennifer Grace <jenniferanngrace@gmail.com>
2018-12-05 22:52:45 -0800

    Fix max call stack size exceeded error in vue cryptos

--- source/default/_patterns/02-molecules/vue-widget/src/vuex-cryptos/index.js
+++ source/default/_patterns/02-molecules/vue-widget/src/vuex-cryptos/index.js
@@ -64,21 +64,25 @@ const getters = {
     switch (filter) {
       // Sort by positive change
       case 'winners':
-        return [...cryptos].sort(
-          ({ percent_change_7d: changeA }, { percent_change_7d: changeB }) =>
-            changeB - changeA
-        );
+        return cryptos
+          .concat()
+          .sort(
+            ({ percent_change_7d: changeA }, { percent_change_7d: changeB }) =>
+              changeB - changeA
+          );
       // Sort by negative change
       case 'losers':
-        return [...cryptos].sort(
-          ({ percent_change_7d: changeA }, { percent_change_7d: changeB }) =>
-            changeA - changeB
-        );
+        return cryptos
+          .concat()
+          .sort(
+            ({ percent_change_7d: changeA }, { percent_change_7d: changeB }) =>
+              changeA - changeB
+          );
       // Filter by "rank" by default
       default:
-        return [...cryptos].sort(
-          ({ rank: rankA }, { rank: rankB }) => rankA - rankB
-        );
+        return cryptos
+          .concat()
+          .sort(({ rank: rankA }, { rank: rankB }) => rankA - rankB);
     }
   },
 };
