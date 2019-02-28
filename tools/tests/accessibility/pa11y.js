fb2b44205fb7eb48f2a1a1bd9da425ef1f0f7400
tjheffner <tannerjheffner@gmail.com>
2019-02-28 10:18:37 -0800

    update paths.assets for drupal, update default pa11y config

--- tools/tests/accessibility/pa11y.js
+++ tools/tests/accessibility/pa11y.js
@@ -1,22 +1,36 @@
-// All PL paths
-const { htmlPaths } = require('../../pl-paths');
+// All non-protons PL paths
+const { componentPaths } = require('../../pl-paths.js');
 
 const plRoot = 'http://0.0.0.0:8080/app-pl/pl'; // @TODO: move this to config
-const urls = htmlPaths.map(partial => `${plRoot}/${partial}`);
+
+// urls comes from particle, includes all demo paths from atoms+
+let urls = componentPaths.map(partial => `${plRoot}/${partial}`);
+
+// prod is any other links that need checked
+const prod = [];
+
+// smash the two urls arrays together
+urls = urls.concat(prod);
 
 // Put together some options to use in each test.
 // See pa11y config https://github.com/pa11y/pa11y-ci#usage
 module.exports = {
-  standard: 'WCAG2AAA',
+  standard: 'WCAG2AA',
   defaults: {
-    concurrency: 5,
-    timeout: 20000,
-    wait: 2000,
+    chromeLaunchConfig: {
+      // this is needed to run in docker
+      args: ["--no-sandbox"]
+    },
+    hideElements: 'img[data-holder-rendered]',
     ignore: [
-      'WCAG2AAA.Principle3.Guideline3_1.3_1_1.H57.2',
+      'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18',
       'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Fail',
-      'WCAG2AA.Principle1.Guideline1_4.1_4_3.G145.Fail',
     ],
+    includeWarnings: false,
+    includeNotices: false,
+    concurrency: 2,
+    timeout: 40000,
+    wait: 2000,
   },
   urls,
 };
