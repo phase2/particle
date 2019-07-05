11d2291991d5a2a4574b05d48f4014eab77051a8
J. Ryan Conklin <jryanconklin@gmail.com>
2019-07-05 15:16:02 -0700

    Update Twig Extensions Via Alter-Twig (#579)
    * WIP: Update Twig Extensions Via Alter-Twig
    
    * Rollback twig components dir move

--- apps/node-pl/pattern-lab/alter-twig.php
+++ apps/node-pl/pattern-lab/alter-twig.php
@@ -3,32 +3,177 @@
 /**
  * @param Twig_Environment $env - The Twig Environment - https://twig.symfony.com/api/1.x/Twig_Environment.html
  * @param $config - Config of `@basalt/twig-renderer`
+ *
  */
-function addCustomExtension(\Twig_Environment &$env, $config) {
+function addFilters(\Twig_Environment &$env, $config) {
 
   /**
-   * @example `<h1>Hello {{ customTwigFunctionThatSaysWorld() }}!</h1>` => `<h1>Hello Custom World</h1>`
+   * Clean Class
+   *
+   * @return string
    */
-//  $env->addFunction(new \Twig_SimpleFunction('customTwigFunctionThatSaysWorld', function () {
-//    return 'Custom World';
-//  }));
+  $clean_class = new Twig_SimpleFilter('clean_class', function ($string) {
+    return $string;
+  });
+  $env->addFilter($clean_class);
 
-  /*
-   * Reverse a string
-   * @param string $theString
-   * @example `<p>{{ reverse('abc') }}</p>` => `<p>cba</p>`
+  /**
+   * Clean ID
+   *
+   * @return string
    */
-//  $env->addFunction(new \Twig_SimpleFunction('reverse', function ($theString) {
-//    return strrev($theString);
-//  }));
+  $clean_id = new Twig_SimpleFilter('clean_id', function ($string) {
+    return $string;
+  });
+  $env->addFilter($clean_id);
 
+  /**
+   * Format Date
+   *
+   * @return string
+   */
+  $format_date = new Twig_SimpleFilter('format_date', function ($string) {
+    return $string;
+  });
+  $env->addFilter($format_date);
 
-//  $env->addExtension(new \My\CustomExtension());
+  /**
+   * Luma
+   *
+   * Take in an rgba associative array return a luminance value
+   * according to ITU-R BT.709.
+   *
+   * @param array $rgba the associative array containing each color value. For example
+   *              array(4) {
+   *                ["r"] => int(0)
+   *                ["g"] => int(123)
+   *                ["b"] => int(255)
+   *                ["a"] => int(1)
+   *              }
+   *
+   * @return float
+   */
+  $luma = new Twig_SimpleFilter('luma', function ($rgba) {
+    // Doesn't handle alpha, yet.
+    return 0.2126 * $rgba['r'] + 0.7152 * $rgba['g'] + 0.0722 * $rgba['b'];
+  });
+  $env->addFilter($luma);
 
-//  `{{ foo }}` => `bar`
-//  $env->addGlobal('foo', 'bar');
+  /**
+   * Placeholder
+   *
+   * @return string
+   */
+  $placeholder = new Twig_SimpleFilter('placeholder', function ($string) {
+    return $string;
+  });
+  $env->addFilter($placeholder);
 
-  // example of enabling the Twig debug mode extension (ex. {{ dump(my_variable) }} to check out the template's available data) -- comment out to disable
-  // $env->addExtension(new \Twig_Extension_Debug());
+  /**
+   * Drupal render filter.
+   *
+   * @return string
+   */
+
+  $render = new Twig_SimpleFilter('render', function ($string) {
+    return $string;
+  });
+  $env->addFilter($render);
+
+  /**
+   * RGBA String
+   *
+   * @return string
+   */
+  $rgba_string = new Twig_SimpleFilter('rgba_string', function ($string) {
+    $rgba = trim(str_replace(' ', '', $string));
+    if (stripos($rgba, 'rgba') !== FALSE) {
+      $res = sscanf($rgba, "rgba(%d, %d, %d, %f)");
+    }
+    else {
+      $res = sscanf($rgba, "rgb(%d, %d, %d)");
+      $res[] = 1;
+    }
+
+    return array_combine(array('r', 'g', 'b', 'a'), $res);
+  });
+  $env->addFilter($rgba_string);
+
+  /**
+   * Safe Join
+   *
+   * @return string
+   */
+  $safe_join = new Twig_SimpleFilter('safe_join', function ($string) {
+    return $string;
+  });
+  $env->addFilter($safe_join);
+
+  /**
+   * Drupal translate filter.
+   *
+   * @return string
+   */
+  $t = new Twig_SimpleFilter('t', function ($string) {
+    return $string;
+  });
+  $env->addFilter($t);
+
+  /**
+   * Without
+   *
+   * @return string
+   */
+  $without = new Twig_SimpleFilter('without', function ($string) {
+    return $string;
+  });
+  $env->addFilter($without);
+
+}
+
+function addFunctions(\Twig_Environment &$env, $config) {
+
+  /**
+   * Link
+   *
+   */
+  $link = new Twig_SimpleFunction(
+    'link',
+    function ($title, $url, $attributes) {
+      if (isset($attributes) && isset($attributes['class'])) {
+        $classes = join(' ', $attributes['class']);
+        return '<a href="' . $url . '" class="' . $classes . '">' . $title . '</a>';
+      }
+      else {
+        return '<a href="' . $url . '">' . $title . '</a>';
+      }
+    },
+    array('is_safe' => array('html'))
+  );
+  $env->addFunction($link);
+
+  /**
+   * Path
+   *
+   */
+  $path = new Twig_SimpleFunction('path', function ($string) {
+    if ($string === '<front>') {
+      return '/';
+    }
+    else {
+      return $string;
+    }
+  });
+  $env->addFunction($path);
+
+  /**
+   * URL
+   *
+   * Https://www.drupal.org/node/2486991.
+   */
+  $url = new Twig_SimpleFunction('url', function ($string) {
+    return '#';
+  });
+  $env->addFunction($url);
 
 }
