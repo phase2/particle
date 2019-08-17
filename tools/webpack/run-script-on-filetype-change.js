04e8738f69bee9971f1124ea912a40832a56d223
Christopher Bloom <bloomcb@gmail.com>
2019-08-16 17:14:52 -0700

    Improved performance for file type change checker

--- tools/webpack/run-script-on-filetype-change.js
+++ tools/webpack/run-script-on-filetype-change.js
@@ -10,40 +10,32 @@ class RunScriptOnFiletypeChange extends ShellHelper {
   apply(compiler) {
     compiler.hooks.emit.tapAsync(
       'RunScriptOnFiletypeChange',
-      (compilation, callback) => {
-        // Convert fileTimestamps to array of arrays to filter it and find
-        // changed files we care about
-
-        const changed = [...compilation.fileTimestamps].filter(([file]) => {
-          // Uncomment to debug
-          // if (file.match(this.options.test)) { console.log(file); }
-
+      ({ fileDependencies, fileTimestamps }, callback) => {
+        // Convert fileTimestamps to array of arrays to find changed files.
+        //  `changed` looks like [filePath, timeStamp]
+        const changed = [...fileTimestamps].find(([file, stamp]) => {
           // Use our file regex to test, eject if not a file we care about
           if (!file.match(this.options.test)) {
             return false;
           }
+          // Bail if the file modified is not actually on the dep chain
+          if (!fileDependencies.has(file)) {
+            return false;
+          }
+
           // Previous file timestamp
           const prevStamp = this.prevTimestamps.get(file) || this.startTime;
           // New timestamp value
-          const nextStamp = compilation.fileTimestamps.get(file) || Infinity;
+          const nextStamp = stamp || Infinity;
           // Only returns true if changed files are newer
           return prevStamp < nextStamp;
         });
 
         // Set stamps to new point
-        this.prevTimestamps = compilation.fileTimestamps;
+        this.prevTimestamps = fileTimestamps;
 
         // If none of the file types have changed, emit as usual
-        if (!changed.length) {
-          callback();
-          return true;
-        }
-
-        // Bail if the file modified is not actually on the dep chain
-        const isDep = changed.some(([changedFilePath]) =>
-          compilation.fileDependencies.has(changedFilePath)
-        );
-        if (!isDep) {
+        if (!changed) {
           callback();
           return true;
         }
