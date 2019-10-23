4c7f86d23702a3620e34f9fb896ee782c471e62c
Christopher Bloom <bloomcb@gmail.com>
2019-10-22 17:03:56 -0700

    sass-loader to ^8

--- webpack.particle.js
+++ webpack.particle.js
@@ -73,7 +73,9 @@ module.exports = {
             loader: 'sass-loader',
             options: {
               sourceMap: true,
-              outputStyle: 'compressed',
+              sassOptions: {
+                outputStyle: 'compressed',
+              },
             },
           },
         ],
