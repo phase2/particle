67d77b66fa940e1fa9383645b72ebd5ae53f9878
Ayana Powell <ayanapowell1@gmail.com>
2020-02-18 16:53:48 -0500

    Override default fontFamily in tailwind.config.js

--- source/default/tailwind.config.js
+++ source/default/tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
       center: true,
       padding: '2rem',
     },
+    fontFamily: {
+      'body': ['Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+      'mono': ['Source Code Pro', 'Courier New', 'monospace'],
+      'serif': ['Oswald', 'Times New Roman', 'serif'],
+    },
     dingo: {
       background: 'red',
       padding: '10px',
