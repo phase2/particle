f2d944de0191e9d745f86e77ff9d56d8693517ab
Jason Leonhard <leonhard.jason@gmail.com>
2020-02-18 10:38:29 -0800

    refactor: use tailwind instead of bootstrap and sass for welcome page

--- source/default/tailwind.config.js
+++ source/default/tailwind.config.js
@@ -1,19 +1,11 @@
+// Default Tailwind config can be found here: https://github.com/tailwindcss/tailwindcss/blob/v1.2.0/stubs/defaultConfig.stub.js
+
 module.exports = {
   theme: {
     container: {
       center: true,
       padding: '2rem',
     },
-    dingo: {
-      background: 'red',
-      padding: '10px',
-      margin: '10px',
-      color: 'white',
-      display: 'flex',
-      flexDirection: 'row',
-      alignContent: 'center',
-    },
-
     extend: {},
   },
   variants: {},
