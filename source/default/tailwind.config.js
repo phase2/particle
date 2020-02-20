0ee55f379ab81dc5fb218fb79911898aa8206b74
Jason Leonhard <leonhard.jason@gmail.com>
2020-02-20 09:02:25 -0800

    feat: add welcome page using tailwind

--- source/default/tailwind.config.js
+++ source/default/tailwind.config.js
@@ -1,3 +1,5 @@
+// Default Tailwind config can be found here: https://github.com/tailwindcss/tailwindcss/blob/v1.2.0/stubs/defaultConfig.stub.js
+
 module.exports = {
   theme: {
     container: {
@@ -5,20 +7,10 @@ module.exports = {
       padding: '2rem',
     },
     fontFamily: {
-      'body': ['Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
-      'mono': ['Source Code Pro', 'Courier New', 'monospace'],
-      'serif': ['Oswald', 'Times New Roman', 'serif'],
+      body: ['Open Sans', 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+      mono: ['Source Code Pro', 'Courier New', 'monospace'],
+      serif: ['Oswald', 'Times New Roman', 'serif'],
     },
-    dingo: {
-      background: 'red',
-      padding: '10px',
-      margin: '10px',
-      color: 'white',
-      display: 'flex',
-      flexDirection: 'row',
-      alignContent: 'center',
-    },
-
     extend: {},
   },
   variants: {},
