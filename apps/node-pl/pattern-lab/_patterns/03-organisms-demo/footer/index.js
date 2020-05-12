bb21712f64beaf94ccbdb5c6b2fb450ebe5791d8
J. Ryan Conklin <jryanconklin@gmail.com>
2020-05-12 13:13:59 -0700

    Update Footer

--- apps/node-pl/pattern-lab/_patterns/03-organisms-demo/footer/index.js
+++ apps/node-pl/pattern-lab/_patterns/03-organisms-demo/footer/index.js
@@ -4,9 +4,7 @@
  * (This file is NOT imported by the design system, but is included as part of
  * a Pattern Lab app.)
  */
-// Import component assets
-// import 'organisms/footer';
-// Import demo assets
+
 import './footers.twig';
 import './footers.yml';
-import './footers.md';
+
