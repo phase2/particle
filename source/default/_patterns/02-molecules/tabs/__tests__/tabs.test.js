5a42960485b51632bbe8f2bbb119f652a2f8279d
Chris Clark <cclark@phase2technology.com>
2020-06-11 10:18:31 -0700

    Add better Jest unit testing.

--- source/default/_patterns/02-molecules/tabs/__tests__/tabs.test.js
+++ source/default/_patterns/02-molecules/tabs/__tests__/tabs.test.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-import tabs, { name } from '..';
+import tabs, { name, selectedIndexState, updateSelectedState } from '..';
 
 const markup = `
 <div class="tabs ">
@@ -38,6 +38,57 @@ const markup = `
 
 </div>
 `;
+const firstSelected = [
+  {
+    id: 'tab-1',
+    panelId: 'panel-1',
+    selected: true,
+  },
+  {
+    id: 'tab-2',
+    panelId: 'panel-2',
+    selected: false,
+  },
+  {
+    id: 'tab-3',
+    panelId: 'panel-3',
+    selected: false,
+  },
+];
+const secondSelected = [
+  {
+    id: 'tab-1',
+    panelId: 'panel-1',
+    selected: false,
+  },
+  {
+    id: 'tab-2',
+    panelId: 'panel-2',
+    selected: true,
+  },
+  {
+    id: 'tab-3',
+    panelId: 'panel-3',
+    selected: false,
+  },
+];
+const thirdSelected = [
+  {
+    id: 'tab-1',
+    panelId: 'panel-1',
+    selected: false,
+  },
+  {
+    id: 'tab-2',
+    panelId: 'panel-2',
+    selected: false,
+  },
+  {
+    id: 'tab-3',
+    panelId: 'panel-3',
+    selected: true,
+  },
+];
 test('tabs component is registered', () => {
   expect(name).toBe('tabs');
 });
@@ -50,6 +101,7 @@ test('First panel components to not have hidden attribute', () => {
 test('panel components have hidden attribute', () => {
   document.body.innerHTML = markup;
   tabs($(document));
+  expect($('#tabs-panel--1239233704-1').prop('hidden')).toBe(true);
   expect($('#tabs-panel--1239233704-2').prop('hidden')).toBe(true);
 });
 test('First tab has aria-selected', () => {
@@ -62,4 +114,31 @@ test('Activate 3rd tab', () => {
   tabs($(document));
   $('#tabs-tab--1239233704-2').trigger('click');
   expect($('#tabs-panel--1239233704-2').prop('hidden')).toBe(false);
+});
+test('should return index of selected tab from tabs array', () => {
+  expect(selectedIndexState(firstSelected)).toBe(0);
+  expect(selectedIndexState(secondSelected)).toBe(1);
+  expect(selectedIndexState(thirdSelected)).toBe(2);
+});
+test('should return object with updated selected tab', () => {
+  const nowSecondSelected = updateSelectedState(1, firstSelected);
+  const nowThirdSelected = updateSelectedState(2, firstSelected);
+  const expectThirdObjectSelected = {
+    id: 'tab-3',
+    panelId: 'panel-3',
+    selected: true,
+  };
+  expect(selectedIndexState(firstSelected)).toBe(0);
+  expect(selectedIndexState(nowSecondSelected)).toBe(1);
+  expect(selectedIndexState(nowThirdSelected)).toBe(2);
+  // Test if object matches expected output.
+  expect(nowThirdSelected[2]).toMatchObject(expectThirdObjectSelected);
+  // Another way to test object structure without exact values.
+  expect(nowThirdSelected[2]).toEqual(
+    expect.objectContaining({
+      id: expect.any(String),
+      panelId: expect.any(String),
+      selected: true,
+    })
+  );
 });
