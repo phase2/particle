90cd0b898f50fcd30dc354c05314a346a4e0a6a0
Christopher Bloom <bloomcb@gmail.com>
2020-06-10 22:14:09 -0700

    Prettier config, massive fmt

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: ['./node_modules/prettier-plugin-twig-melody'],
+  proseWrap: 'always',
   singleQuote: true,
   twigAlwaysBreakObjects: true,
   twigOutputEndblockName: true,
