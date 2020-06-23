ea562ce9bc591130304efa384d7db1021fa29f05
J. Ryan Conklin <jryanconklin@gmail.com>
2020-06-23 11:02:54 -0700

    Feature: Remove JS Boilerplate

--- source/default/_patterns/01-atoms/form-element/index.js
+++ source/default/_patterns/01-atoms/form-element/index.js
@@ -2,6 +2,7 @@
  * form-element
  */
 
+// eslint-disable-next-line
 import $ from 'jquery';
 
 // Module dependencies
@@ -14,42 +15,9 @@ import './_textarea.twig';
 
 export const name = 'form-element';
 
-export const defaults = {
-  dummyClass: 'js-form-element-exists',
-};
+export function disable() {}
 
-/**
- * Components may need to run clean-up tasks if they are removed from DOM.
- *
- * @param {jQuery} $context - A piece of DOM
- * @param {Object} settings - Pertinent settings
- */
-// eslint-disable-next-line no-unused-vars
-export function disable($context, settings) {}
-
-/**
- * Each component has a chance to run when its enable function is called. It is
- * given a piece of DOM ($context) and a settings object. We destructure our
- * component key off the settings object and provide an empty object fallback.
- * Incoming settings override default settings via Object.assign().
- *
- * @param {jQuery} $context - A piece of DOM
- * @param {Object} settings - Settings object
- */
-export function enable($context, { formElement = {} }) {
-  // Find our component within the DOM
-  const $formElement = $('.form-element', $context);
-  // Bail if component does not exist
-  if (!$formElement.length) {
-    return;
-  }
-  // Merge defaults with incoming settings
-  const settings = {
-    ...defaults,
-    ...formElement,
-  };
-  // An example of what could be done with this component
-  $formElement.addClass(settings.dummyClass);
-}
+// eslint-disable-next-line
+export function enable($context) {}
 
 export default enable;
