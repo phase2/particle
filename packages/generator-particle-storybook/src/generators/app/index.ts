a2acc5e019a90672b42a0a1ed5553ce1f889caf4
Shawn Mealey <mealeyst@gmail.com>
2020-08-21 13:26:57 -0400

    WIP Adding storybook specific prompts

--- packages/generator-particle-storybook/src/generators/app/index.ts
+++ packages/generator-particle-storybook/src/generators/app/index.ts
@@ -1,22 +1,61 @@
-import { preview } from './templates/preview'
 import Generator from 'yeoman-generator'
-import { white } from 'chalk'
+import merge from 'lodash.merge'
 import fs from 'fs'
 
 import { Answers, FrontendFrameworkOptions } from '@phase2/particle-types'
-import { main } from './templates/main'
 
-export const storybookAddons: string[] = [
-  '@storybook/addon-knobs',
-  '@storybook/addon-actions',
-  '@storybook/addon-links',
-  '@storybook/addon-viewport',
-  '@storybook/addon-a11y',
+type FrontendOptionsMap = { [key in FrontendFrameworkOptions]: string }
+
+interface LooseObject {
+  [key: string]: any
+}
+
+const sbVersion = '^5.3.19'
+export const storybookAddons = [
+  {
+    name: 'knobs',
+    dependency: { '@storybook/addon-knobs': sbVersion },
+    checked: true,
+  },
+  {
+    name: 'actions',
+    dependency: { '@storybook/addon-actions': sbVersion },
+    checked: true,
+  },
+  {
+    name: 'links',
+    dependency: { '@storybook/addon-links': sbVersion },
+    checked: true,
+  },
+  {
+    name: 'viewport',
+    dependency: { '@storybook/addon-viewport': sbVersion },
+    checked: true,
+  },
+  {
+    name: 'a11y',
+    dependency: { '@storybook/addon-a11y': sbVersion },
+    checked: true,
+  },
+  {
+    name: 'docs',
+    dependency: { '@storybook/addon-docs': sbVersion },
+    checked: true,
+  },
+  {
+    name: 'backgrounds',
+    dependency: { '@storybook/addon-backgrounds': sbVersion },
+    checked: true,
+  },
 ]
 
-const storybookSupportedVersion = '^5.3.19'
 const storybookPath = 'app/storybook'
-const storiesRoot: string[] = ['./stories/**/*.story.tsx']
+
+const FrontendOptions: FrontendOptionsMap = {
+  [FrontendFrameworkOptions.REACT]: '@storybook/react',
+  [FrontendFrameworkOptions.WEBCOMPONENTS]: '@storybook/web-components',
+  [FrontendFrameworkOptions.TWIG]: 'storybook-twig',
+}
 
 /**
  * @assumption we are already inside the particle root directory
@@ -25,55 +64,113 @@ const storiesRoot: string[] = ['./stories/**/*.story.tsx']
 module.exports = class extends Generator {
   configuration: Answers
   updatePackageJson: (newJson: Record<string, any>) => void
+  framework: FrontendFrameworkOptions
+  props: {
+    storybook_type: FrontendFrameworkOptions
+    storybook_addons: string[]
+  }
   constructor(args: any, opts: any) {
     super(args, opts)
     this.configuration = opts.configuration
     this.updatePackageJson = opts.updatePackageJson
+    this.framework = opts.framework
+    this.props = {
+      storybook_type: opts.framework,
+      storybook_addons: [],
+    }
   }
 
-  addStorybookDependencies() {
-    // TODO to add support for other frameworks
-    const dependencies = ['@storybook/react', ...storybookAddons]
-
-    console.log(white('adding storybook dependencies to the packageJson'))
-
-    // if noInstall flag is passed assume that updatePackageJson was passed and fire off command
-    // else run npm install
-    this.updatePackageJson({
-      scripts: {
-        'build:storybook': 'build-storybook -c ./apps/storybook',
-        'dev:storybook': 'start-storybook -p 6006 -c ./apps/storybook',
+  async prompting() {
+    const frameworkPrompt = {
+      type: 'list',
+      name: 'storybook_type',
+      message: 'What will you be using Storybook for?',
+      choices: Object.values(FrontendFrameworkOptions),
+    }
+    const prompts = [
+      ...(!this.props.storybook_type ? [frameworkPrompt] : []),
+      {
+        type: 'checkbox',
+        name: 'storybook_addons',
+        message: 'What addons would you like to use (all enabled by default)',
+        choices: storybookAddons,
       },
-      devDependencies: dependencies.reduce<Record<string, string>>(
-        (acc, value: string) => {
-          acc[value] = storybookSupportedVersion
-          return acc
-        },
-        {}
-      ),
+    ]
+    const answers = await this.prompt(prompts)
+    this.props = { ...this.props, ...answers }
+  }
+
+  configuring() {
+    console.log(this.props)
+    let devDependencies: LooseObject = {}
+    storybookAddons.forEach((addon) => {
+      if (this.props.storybook_addons.includes(addon.name)) {
+        const dependency = Object.keys(addon.dependency)[0]
+        const version = Object.values(addon.dependency)[0]
+        devDependencies[dependency] = version
+      }
     })
+    const storybookTypeKey: string = FrontendOptions[this.props.storybook_type]
+    const pkgJson = {
+      devDependencies: {
+        ...devDependencies,
+        [storybookTypeKey]: sbVersion,
+      },
+    }
+
+    // merge(pkgJson.devDependencies, { [storybookTypeKey]: sbVersion })
+    this.fs.extendJSON(this.destinationPath('package.json'), pkgJson)
+    // this.fs.writeJSON('./package.json', pkgJson)
+  }
+  // }
+
+  install() {
+    this.npmInstall()
   }
 
-  async createStorybookFiles() {
-    console.log(white('creating files & folders for storybook'))
+  // addStorybookDependencies() {
+  //   // TODO to add support for other frameworks
+  //   const dependencies = ['@storybook/react', ...storybookAddons]
 
-    // create the folders
-    fs.mkdirSync(`${process.cwd()}/${storybookPath}/`, { recursive: true })
+  //   console.log(white('adding storybook dependencies to the packageJson'))
 
-    // create the files
-    fs.writeFileSync(
-      this.destinationPath(`${storybookPath}/main.js`),
-      main({
-        addons: storybookAddons,
-        componentLibraryPath: `../../${this.configuration.componentLibraryPath}`,
-        storiesRoot,
-      })
-    )
-    fs.writeFileSync(
-      this.destinationPath(`${storybookPath}/preview.js`),
-      preview({
-        frontendFramework: FrontendFrameworkOptions.REACT,
-      })
-    )
-  }
+  //   // if noInstall flag is passed assume that updatePackageJson was passed and fire off command
+  //   // else run npm install
+  //   this.updatePackageJson({
+  //     scripts: {
+  //       'build:storybook': 'build-storybook -c ./apps/storybook',
+  //       'dev:storybook': 'start-storybook -p 6006 -c ./apps/storybook',
+  //     },
+  //     devDependencies: dependencies.reduce<Record<string, string>>(
+  //       (acc, value: string) => {
+  //      clear   acc[value] = storybookSupportedVersion
+  //         return acc
+  //       },
+  //       {}
+  //     ),
+  //   })
+  // }
+
+  // async createStorybookFiles() {
+  //   console.log(white('creating files & folders for storybook'))
+
+  //   // create the folders
+  //   fs.mkdirSync(`${process.cwd()}/${storybookPath}/`, { recursive: true })
+
+  //   // create the files
+  //   fs.writeFileSync(
+  //     this.destinationPath(`${storybookPath}/main.js`),
+  //     main({
+  //       addons: storybookAddons,
+  //       componentLibraryPath: `../../${this.configuration.componentLibraryPath}`,
+  //       storiesRoot,
+  //     })
+  //   )
+  //   fs.writeFileSync(
+  //     this.destinationPath(`${storybookPath}/preview.js`),
+  //     preview({
+  //       frontendFramework: FrontendFrameworkOptions.REACT,
+  //     })
+  //   )
+  // }
 }
