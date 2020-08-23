c198ed75c077ca5d1b7007769daac86768e991dc
Bernardo Martinez <bernardo-martinez@utc.edu>
2020-08-23 17:14:49 -0400

    updated colors and styles to reflect a similar experience to the current site

--- source/default/_patterns/00-protons/index.js
+++ source/default/_patterns/00-protons/index.js
@@ -3,6 +3,16 @@
  */
 
 import './tailwind.tokens.css';
+
+//Legacy
+
+//Legacy CSS
+import './legacy/css/footer.css';
+import "./legacy/css/global.css";
+import "./legacy/css/utc-sidebar-menu.css";
+import "./legacy/css/utc_department_info.css";
+
+//Legacy JS
 import './legacy/js/utc-sidebar-menu.js';
 
 // Export global variables.
