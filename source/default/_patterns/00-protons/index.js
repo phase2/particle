d14f2c5de6fe96047cdbd32237fbc8001675e2f9
Bernardo Martinez <bernardo-martinez@utc.edu>
2020-08-24 00:22:52 -0400

    transferred more scss to css styles

--- source/default/_patterns/00-protons/index.js
+++ source/default/_patterns/00-protons/index.js
@@ -11,6 +11,12 @@ import './legacy/css/footer.css';
 import "./legacy/css/global.css";
 import "./legacy/css/utc-sidebar-menu.css";
 import "./legacy/css/utc_department_info.css";
+import "./legacy/css/layout-builder.css";
+import "./legacy/css/off-canvas.css";
+import "./legacy/css/components/header/_utc_custom_header.css";
+import "./legacy/css/components/header/_main-navigation.css";
+// import "./legacy/css/components/field/";
+
 
 //Legacy JS
 import './legacy/js/utc-sidebar-menu.js';
