8692a8e3423c0e1bc13c0e80ee53e34681fc38bf
Bernardo Martinez <bernardo-martinez@utc.edu>
2020-08-25 11:14:38 -0400

    added new stylessheets to index.js

--- source/default/_patterns/00-protons/index.js
+++ source/default/_patterns/00-protons/index.js
@@ -15,6 +15,14 @@ import "./legacy/css/layout-builder.css";
 import "./legacy/css/off-canvas.css";
 import "./legacy/css/components/header/_utc_custom_header.css";
 import "./legacy/css/components/header/_main-navigation.css";
+import "./legacy/css/components/card/_card.css";
+import "./legacy/css/components/buttons/_buttons.css";
+import "./legacy/css/components/section/_section.css";
+import "./legacy/css/components/UTC-custom-blocks/_utc_blockquotes.css";
+import "./legacy/css/components/UTC-custom-blocks/_utc_employee.css";
+import "./legacy/css/components/UTC-custom-blocks/_utclib-item-card.css";
+import "./legacy/css/components/UTC-custom-blocks/_utc-button-group.css";
+// import "./legacy/css/components/UTC-custom-blocks/";
 // import "./legacy/css/components/field/";
 
 
