a5848b804e74f41a4c93cfcd86a6b0491310b50f
Shawn Mealey <mealeyst@gmail.com>
2020-08-26 13:20:47 -0400

    Updating the Stylelint config rules

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,24 +1,28 @@
 module.exports = {
-    extends: [
-        'stylelint-config-prettier'
+  extends: ['stylelint-config-recommended', 'stylelint-config-prettier'],
+  rules: {
+    indentation: 2,
+    'declaration-colon-space-after': 'always',
+    'declaration-no-important': true,
+    'max-nesting-depth': 3,
+    'selector-max-specificity': '0,3,3',
+    'selector-max-id': 0,
+    'at-rule-no-vendor-prefix': true,
+    'media-feature-name-no-vendor-prefix': true,
+    'property-no-vendor-prefix': true,
+    'selector-no-vendor-prefix': true,
+    'value-no-vendor-prefix': true,
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'responsive',
+          'variants',
+          'screen',
+        ],
+      },
     ],
-    plugins: [
-        'stylelint-scss',
-    ],
-    rules: {
-        'indentation': 2,
-        'declaration-colon-space-after': 'always',
-        'declaration-no-important': true,
-        'max-nesting-depth': 3,
-        'selector-max-specificity': '0,3,3',
-        'selector-max-id': 0,
-        'scss/at-extend-no-missing-placeholder': true,
-        'scss/selector-no-redundant-nesting-selector': true,
-        'at-rule-no-vendor-prefix': true,
-        'media-feature-name-no-vendor-prefix': true,
-        'property-no-vendor-prefix': true,
-        'selector-no-vendor-prefix': true,
-        'value-no-vendor-prefix': true
-    },
-};
-//# sourceMappingURL=index.js.map
+  },
+}
