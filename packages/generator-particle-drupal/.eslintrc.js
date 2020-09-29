26a4f7ed30d0dee5f1c75d67a474f94a4d9ae0fe
J. Ryan Conklin <jryanconklin@gmail.com>
2020-09-28 17:18:13 -0700

    Eleven: progress prompting on theme

---
+++ packages/generator-particle-drupal/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+  },
+};
--- packages/generator-particle-drupal/generators/app/index.js
+++ packages/generator-particle-drupal/generators/app/index.js
@@ -1,25 +1,76 @@
 const Generator = require('yeoman-generator');
 
-const themeNameTitle = '';
-const themeNamePascal = '';
-const themeNameSnake = '';
-const themeNameKebab = '';
-const themeNameDescription = '';
-const drupalDist = '';
+// Module Dependencies
+const chalk = require('chalk');
+const { camelCase, kebabCase, snakeCase, startCase, upperFirst } = require('lodash');
+const path = require('path');
+const fs = require('fs');
+
+// Logo for Terminal Output
+const logo = require('../../logo')
 
 module.exports = class extends Generator {
+  // Initialize config from Particle if it exists.
+  initializing() {
+    try {
+      const config = fs.readFileSync(path.join(this.destinationPath(), '.particlerc.json'), 'utf8')
+      const { projectName, drupalThemePath, drupalDist } = JSON.parse(config);
+      this.config.defaults({projectName, drupalThemePath, drupalDist});
+    } catch (e) {
+      this.log(chalk.bgRed(`Skipping Particle Defaults: ${e}`))
+    }
+  }
+
   // Prompts
   async prompting() {
-    this.log("Welcome to Particle's Drupal Theme Generator");
-
-    const answers = await this.prompt([
+    this.log(logo);
+    this.log(chalk.bgBlue(`Welcome to Particle's Drupal Theme Generator`));
+    const answers = [
       {
         type: 'input',
         name: 'themeName',
         message: 'What would you like to name your theme?',
-        // Supply default-theme name from other project root config?
-        default: 'particle',
+        default: this.config.get('projectName') ? `${this.config.get('projectName')} Theme` : 'Particle',
       },
-    ]);
+      {
+        type: 'input',
+        name: 'themeDescription',
+        message: 'What description should we give your theme?',
+        default: this.config.get('themeDescription'),
+      },
+      {
+        type: 'input',
+        name: 'drupalThemePath',
+        message: 'Where should we write your Drupal theme?',
+        default: this.config.get('drupalThemePath'),
+      },
+      {
+        type: 'input',
+        name: 'drupalDist',
+        message: 'Where relative path from the drupal theme will you compile drupal assets (ex: "/dist")?',
+        default: this.config.get('drupalDist'),
+      },
+    ];
+
+    return this.prompt(answers).then((props) => {
+      this.props = {
+        ...props,
+        themeNameTitle: startCase(props.themeName),
+        themeNamePascal: upperFirst(camelCase(props.themeName)),
+        themeNameSnake: snakeCase(props.themeName),
+        themeNameKebab: kebabCase(props.themeName),
+      };
+    });
+
+
+
+  }
+
+  writing() {
+    // Update our yo configuration from props.
+    this.config.set(this.props);
+
+
+    this.log(chalk.bgGreen(`Your Drupal Theme "${this.props.themeNameKebab}" has been created!`));
   }
 };
