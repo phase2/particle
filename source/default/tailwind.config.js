f8c18d20889999d3761ff71c951e1f1f2f8e07e4
Bridget Hornsby <82905787+UTCbridget@users.noreply.github.com>
2021-09-24 10:02:39 -0400

    UTC-330: Add image hover component.
    UTC-330: Add image hover component.

--- source/default/tailwind.config.js
+++ source/default/tailwind.config.js
@@ -41,7 +41,10 @@ module.exports = {
         'utchero': '1fr 60% 35% 1fr',
         'utcheroright': '1fr 35% 60% 1fr',
         'utcherocenter': '1fr 45% 45% 1fr',
-      }
+      },
+      width: {
+        '95per': '95%',
+      },
     },
     minHeight: {
       '23': '23rem'
