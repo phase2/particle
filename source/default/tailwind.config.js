869eee93fcbebe7259c0704bcef234acf40c2262
Bridget Hornsby <82905787+UTCbridget@users.noreply.github.com>
2021-10-26 17:03:06 -0400

    UTC-330: Remove tailwind.config.js as it is not changed for this PR.

--- source/default/tailwind.config.js
+++ source/default/tailwind.config.js
@@ -34,9 +34,10 @@ module.exports = {
         // Adds a custom template for the utc hero block
         'utchero': '40px 1fr 1fr 70px',
         'utcheroreverse': '70px 1fr 1fr 40px',
-        'utcherocenter': '25px 1fr 1fr 25px',       
+        'utcherocenter': '25px 1fr 1fr 25px',
         // Adds a custom template for the highlight block
         'utchighlight': '40px 1fr 1fr 40px 40px',
+
       },
       gridTemplateColumns: {
         // Adds a custom template for the utc hero block
