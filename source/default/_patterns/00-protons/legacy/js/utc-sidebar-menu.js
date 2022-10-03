7dee4bd16126772b66600be5685567d7d3335a95
Bridget Hornsby <82905787+UTCbridget@users.noreply.github.com>
2022-10-03 15:42:14 -0400

    UTC-2216: Fix sidebar js file.

--- source/default/_patterns/00-protons/legacy/js/utc-sidebar-menu.js
+++ source/default/_patterns/00-protons/legacy/js/utc-sidebar-menu.js
@@ -3,21 +3,9 @@
 
     Drupal.behaviors.sidebarmenu = {
         attach: function(context, settings) {
-
-            $(".sidebar-menu li a").each(function() {
-                if ($(this).next().length > 0) {
-                    $(this).addClass("parent");
-                };
-            })
-            var menux = $('.sidebar-menu li a.parent');
-            if ($('.more').length === 0) {
-                $('<div aria-hidden="true" class="more closed"><i class="fas fa-angle-right"></i></div>').insertBefore(menux);
-            };
-            $('.menu-btn').click(function() {
-                $('nav').toggleClass('menu-open');
-            });
+            $('<div aria-hidden="true" class="more closed"><i class="fas fa-angle-right"></i></div>').prependTo('.menu-item--expanded');
             $('.menu-item--expanded.menu-item--active-trail').addClass('open');
-            $('.menu-item--expanded.menu-item--active-trail.open > div.more').removeClass('closed').addClass('open');
+            $('.menu-item--expanded.menu-item--active-trail > .closed').removeClass('closed').addClass('open');
             $(document).delegate('.more.open', 'click', function(){
                 $(this).removeClass('open').addClass('closed');
                 $(this).parent().removeClass('open');
