b1a50533e8f76f7adde5fae69ae4ffe84654ff36
Bridget Hornsby <82905787+UTCbridget@users.noreply.github.com>
2022-10-05 13:47:08 -0400

    UTC-2265: Clean up on aisle 5.

--- source/default/_patterns/00-protons/legacy/js/utc-sidebar-menu.js
+++ source/default/_patterns/00-protons/legacy/js/utc-sidebar-menu.js
@@ -3,19 +3,15 @@
 
     Drupal.behaviors.sidebarmenu = {
         attach: function(context, settings) {
-            $('<div aria-hidden="true" class="more closed"><i class="fas fa-angle-right"></i></div>').prependTo('.menu-item--expanded');
-            $('.menu-item--expanded.menu-item--active-trail').addClass('open');
-            
-            $(document).delegate('.more.open', 'click', function(){
+                
+            $(document).delegate('.utc-sidebar .more.open', 'click', function(){
                 $(this).removeClass('open').addClass('closed');
                 $(this).parent().removeClass('open');
             });
-            $(document).delegate('.more.closed', 'click', function(){
+            $(document).delegate('.utc-sidebar .more.closed', 'click', function(){
                 $(this).removeClass('closed').addClass('open');
                 $(this).parent().addClass('open');
             });
-            $('.menu-item--expanded.menu-item--active-trail.open>.more').click();
-            $('.menu-item--expanded.menu-item--active-trail.open>.more .more.open').removeClass('open').addClass('closed');
         }
     };
 }(jQuery, Drupal, drupalSettings));
