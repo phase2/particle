101259ab9e81803495f8549522dd19107e3d4f9a
Bridget Hornsby <82905787+UTCbridget@users.noreply.github.com>
2023-05-26 12:44:15 -0400

    UTC-2473: This PR checks for need for js functions before calling.

--- source/default/_patterns/00-protons/legacy/js/back-to-top-button.js
+++ source/default/_patterns/00-protons/legacy/js/back-to-top-button.js
@@ -4,26 +4,32 @@
     Drupal.behaviors.backtotopbutton = {
         attach: function(context, settings) {
             let scrollButton = document.getElementById("scroll-to-top-btn");
-            
-            // When the user scrolls down 1500px from the top of the document, show the button
-            window.onscroll = function() {scrollFunction()};
             scrollButton.style.display = "none";
-            function scrollFunction() {
-              if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
-                scrollButton.style.display = "flex";
-              } else {
-                scrollButton.style.display = "none";
-              }
-            }
             
-            // When the user clicks on the button, scroll to the top of the document
-            function topFunction() {
-              document.body.scrollTop = 0;
-              document.documentElement.scrollTop = 0;
+            //check to see if the window is tall enough for a back-to-top button
+            function setWindowHeight(){
+              var windowHeight = window.innerHeight;
+                  if (windowHeight > 1500) {
+                    // When the user scrolls down 1500px from the top of the document, show the button
+                    window.onscroll = function() {scrollFunction()};
+                    
+                    window.addEventListener("resize",setWindowHeight,false);
+                      function scrollFunction() {
+                        if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
+                          scrollButton.style.display = "flex";
+                        } else {
+                          scrollButton.style.display = "none";
+                        }
+                      }
+                      // When the user clicks on the button, scroll to the top of the document
+                      function topFunction() {
+                        document.body.scrollTop = 0;
+                        document.documentElement.scrollTop = 0;
+                      }
+                  scrollButton.addEventListener("click", topFunction);
+                }
             }
-
-            scrollButton.addEventListener("click", topFunction);
-           
+            setWindowHeight();
         }
     };
   }(jQuery, Drupal, drupalSettings));
