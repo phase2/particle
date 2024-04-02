35b466b52a00bdf55f1152a43c7fed024ea6a53c
Bridget Hornsby <82905787+UTCbridget@users.noreply.github.com>
2024-04-02 15:00:38 -0400

    UTC-628: Numerous MarCom edits.

--- source/default/_patterns/00-protons/legacy/js/utc-academic-programs.js
+++ source/default/_patterns/00-protons/legacy/js/utc-academic-programs.js
@@ -1,51 +1,50 @@
 
 
 (function($, Drupal, drupalSettings) {
-    "use strict";
-    Drupal.behaviors.academicprograms = {
-        attach: function(context, settings) {
-          //console.log("utc-academic-programs.js is loaded.")
-            if (document.location.pathname.indexOf("/academic-affairs/registrar/utc-programs") == 0) {
-                document.body.className = "utc-programs-page";
-            }
-            
-            var programOverlay = document.querySelector(".utc-programs-page .program-overlay");
+  "use strict";
+  Drupal.behaviors.academicprograms = {
+      attach: function(context, settings) {
+        //console.log("utc-academic-programs.js is loaded.")
+          if (window.location.href.indexOf("majors-overview") > -1) {
+            $('body').addClass('utc-programs-page');
+          }
+          var programHeadline = $(".utc-programs-page .program-page-title-and-crumbs");
+          var programOverlay = $(".utc-programs-page .program-overlay");
 
-            $(programOverlay).css({"z-index":"-1","opacity":".75"});
-            $('.utcloadingcontainer').css("display","none");
+          $(programOverlay).css({"z-index":"-1","opacity":".75"});
+          $('.utcloadingcontainer').css("display","none");
 
-            function ghostMainContent(){
-                programOverlay.style.zIndex= "2";
-            }
-            function unghostMainContent(){
-                programOverlay.style.zIndex= "-1";
-            }
-            function togglediv(id) {
-              document.querySelectorAll(".offscreen-program-details").forEach(function(div) {
-                if (div.id == id) {
-                  // Toggle specified DIV
-                  div.style.right = div.style.right == "0" ? "-650px" : "0";
-                } else {
-                  // Hide other DIVs
-                  div.style.right = "-650px";
-                }
-                ghostMainContent()
-              });
-            }
-            $(".program-btn").each(function(){
-              var getProgramBtnDetailId = $(this).attr('data-src');
-              $(this).on("click",function(){
-                togglediv(getProgramBtnDetailId);
-              });
+          function ghostMainContent(){
+             $(programHeadline).css('opacity','.25');
+              $(programOverlay).css('z-index','2');
+          }
+          function unghostMainContent(){
+            $(programHeadline).css('opacity','1');
+            $(programOverlay).css('z-index','-1');
+          }
+          function togglediv(getProgramBtnDetailId) {
+            $(".offscreen-program-details").each(function(){
+              var detailId= $(this).attr('id');
+              if (detailId == getProgramBtnDetailId){
+                $('.offscreen-program-details').css('right','-650px').removeClass('detail-open');
+                $(this).css('right','0').addClass('detail-open');
+              } 
+              ghostMainContent();
             });
-            $(".close-btn").each(function(){
-              var parentId = $(this).closest('.offscreen-program-details').attr('id');
-              $(this).on("click",function(){
-                $("#"+parentId).css("right","-650px");
-                unghostMainContent();
-              });
+          }
+          $(".program-btn").each(function(){
+            var getProgramBtnDetailId = $(this).attr('data-src');
+            $(this).on("click",function(){
+              togglediv(getProgramBtnDetailId);
             });
-        }
-    };
+          });
+          $(".close-btn").each(function(){
+            var parentId = $(this).closest('.offscreen-program-details').attr('id');
+            $(this).on("click",function(){
+              $("#"+parentId).css("right","-650px");
+              unghostMainContent();
+            });
+          });
+      }
+  };
 }(jQuery, Drupal, drupalSettings));
-  
