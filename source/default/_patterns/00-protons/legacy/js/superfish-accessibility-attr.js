421737d590a188947a2204ff15370e3575b5d74c
Bridget Hornsby <82905787+UTCbridget@users.noreply.github.com>
2024-08-19 15:55:54 -0400

    UTC-673: Update css and js to solve some linting issues.

--- source/default/_patterns/00-protons/legacy/js/superfish-accessibility-attr.js
+++ source/default/_patterns/00-protons/legacy/js/superfish-accessibility-attr.js
@@ -1,38 +1,37 @@
+(function ($, Drupal, drupalSettings) {
+  'use strict';
 
-(function($, Drupal, drupalSettings) {
-    "use strict";
-
-    Drupal.behaviors.superfishaccessibilityattributes = {
-        attach: function(context, settings) {
-            //console.log("sf accessibility js loaded.");
-            var getSfLink = document.querySelectorAll(".accessibility-link-attributes");
-            var i;
-            for (i = 0; i < getSfLink.length; i++) { 
-                getSfLink[i].title = getSfLink[i].textContent;
-                getSfLink[i].setAttribute("role", "link");
-                getSfLink[i].setAttribute("aria-label", getSfLink[i].textContent);
-            }
-            var getSfLink2 = document.querySelectorAll(".twm-link");
-            var i;
-            for (i = 0; i < getSfLink2.length; i++) { 
-                getSfLink2[i].title = getSfLink2[i].textContent;
-                getSfLink2[i].setAttribute("role", "link");
-                getSfLink2[i].setAttribute("aria-label", getSfLink2[i].textContent);
-            }
-            var getMobileBtn = document.querySelector("#mobile-menu-icon");
-            var getMobileMenu = document.querySelector("#mobile-menu");
-            
-            function mobileIconAccessibilityAttr(){
-                if (window.matchMedia("(max-width: 768px)").matches) {
-                    getMobileBtn.setAttribute("aria-hidden", "false");
-                    getMobileMenu.setAttribute("aria-hidden", "false");
-                } 
-            }
-            mobileIconAccessibilityAttr();
-
-            window.addEventListener("resize", function() {
-                mobileIconAccessibilityAttr();
-            });
+  Drupal.behaviors.superfishaccessibilityattributes = {
+    attach: function (context, settings) {
+      const getSfLink = document.querySelectorAll(
+        '.accessibility-link-attributes'
+      );
+      let i;
+      const getSfLink2 = document.querySelectorAll('.twm-link');
+      let j;
+      const getMobileBtn = document.querySelector('#mobile-menu-icon');
+      const getMobileMenu = document.querySelector('#mobile-menu');
+      for (i = 0; i < getSfLink.length; i++) {
+        getSfLink[i].title = getSfLink[i].textContent;
+        getSfLink[i].setAttribute('role', 'link');
+        getSfLink[i].setAttribute('aria-label', getSfLink[i].textContent);
+      }
+      for (j = 0; j < getSfLink2.length; j++) {
+        getSfLink2[j].title = getSfLink2[j].textContent;
+        getSfLink2[j].setAttribute('role', 'link');
+        getSfLink2[j].setAttribute('aria-label', getSfLink2[j].textContent);
+      }
+      function mobileIconAccessibilityAttr() {
+        if (window.matchMedia('(max-width: 768px)').matches) {
+          getMobileBtn.setAttribute('aria-hidden', 'false');
+          getMobileMenu.setAttribute('aria-hidden', 'false');
         }
-    };
-}(jQuery, Drupal, drupalSettings));
+      }
+      mobileIconAccessibilityAttr();
+
+      window.addEventListener('resize', function () {
+        mobileIconAccessibilityAttr();
+      });
+    },
+  };
+})(jQuery, Drupal, drupalSettings);
