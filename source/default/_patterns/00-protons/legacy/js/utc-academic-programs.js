2a9d2538f1de6c0b15c5fedca21c1c196046579a
Bridget Hornsby <82905787+UTCbridget@users.noreply.github.com>
2024-09-11 14:27:31 -0400

    UTC-672: Fix body tag injection on wrong pages.

--- source/default/_patterns/00-protons/legacy/js/utc-academic-programs.js
+++ source/default/_patterns/00-protons/legacy/js/utc-academic-programs.js
@@ -8,7 +8,7 @@
         $('body').addClass('utc-programs-page');
       }
       var programBlock = document.getElementsByClassName('utc-programs-block');
-      if (programBlock) {
+      if ($(programBlock).length > 0) {
         $('body').addClass('utc-programs-page');
       }
       $(window).scroll(function () {
@@ -76,30 +76,30 @@
 
       /***JS for the video modal window */
       // Get the modal
-      var modal = document.getElementById("dp-modal");
+      var modal = document.getElementById('dp-modal');
 
       // Get the button that opens the modal
-      var btn = document.getElementById("dp-modal-button");
+      var btn = document.getElementById('dp-modal-button');
 
       // Get the <span> element that closes the modal
-      var span = document.getElementsByClassName("close-modal")[0];
+      var span = document.getElementsByClassName('close-modal')[0];
 
-      // When the user clicks the button, open the modal 
-      btn.onclick = function() {
-        modal.style.display = "flex";
-      }
+      // When the user clicks the button, open the modal
+      btn.onclick = function () {
+        modal.style.display = 'flex';
+      };
 
       // When the user clicks on <span> (x), close the modal
-      span.onclick = function() {
-        modal.style.display = "none";
-      }
+      span.onclick = function () {
+        modal.style.display = 'none';
+      };
 
       // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
+      window.onclick = function (event) {
         if (event.target == modal) {
-          modal.style.display = "none";
+          modal.style.display = 'none';
         }
-      }
+      };
     },
   };
 })(jQuery, Drupal, drupalSettings);
